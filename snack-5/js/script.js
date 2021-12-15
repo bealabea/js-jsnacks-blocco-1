75d95b5471cccb1c2df2a0442e1b117a77fe1b49
bealabea <beatricemarin89@yahoo.it>
2021-12-15 20:28:57 +0100

    Js snack 5 fix

--- snack-5/js/script.js
+++ snack-5/js/script.js
@@ -7,19 +7,15 @@
 let numList = [];
 // creo una variabile per la somma con valore zero di default
 let sum = 0;
-// creo una variabile undefined che andrà a popolare l'utente con il prompt
-let number;
-// creo una variabile con valore zero per il ciclo
-let i= 0;
 
 // creo un ciclo Do While: chiedo all'utente un numero, aggiungo il numero all'array vuoto,
 // sommo i numeri aggiunti dall'utente nell'array e continuo il ciclo fino a che la somma dei numeri dati dall'utente sta sotto 50 (condizione while) 
 do {
-    number = prompt('inserisci un numero');
+    let number = parseInt(prompt('inserisci un numero'));
     numList.push(number);
-    sum += parseInt(numList[i]);
+    sum += parseInt(number);
+    console.log(number);
     console.log(numList);
-    i++;
 } while(sum < 50);
 
 console.log(sum);
