37a1ce8021e2e8ad1d09873041490f7ce52bafb2
bealabea <beatricemarin89@yahoo.it>
2021-12-15 18:35:57 +0100

    js snack 4 fix

--- snack-4/js/script.js
+++ snack-4/js/script.js
@@ -6,7 +6,7 @@ const userNum = (prompt('scrivi un numero di 4 cifre e lo sommerò'));
 // creo una variabile della somma a cui attribuisco un valore di default
 let sum = 0;
 // creo un ciclo for in cui dico di sommare tutti gli elementi della stringa dell'utente che trasformo in numero con il parseInt.
-for(i = 0; i < userNum.length; i++){
+for(let i = 0; i < userNum.length; i++){
     sum += parseInt(userNum[i]);
 }
 // stampo il totale
